36664af75e5a8834a717a4a82e6adac999ce8cc7
Mahdi Kleit <46169379+Kleitoun@users.noreply.github.com>
2023-03-20 18:37:12 -0400

    Update coarse2fine.py

--- code/coarse2fine.py
+++ code/coarse2fine.py
@@ -209,7 +209,7 @@ class C2F(torch.nn.Module):
 			prediction_test = self.fine_forward3(v_test_x3s, v_test_x3w)
 			pre_labels = [Max_Index(line) for line in prediction_test.data.numpy()]
 			recall, precision, macrof1, microf1, acc = Get_Report(test_y3, pre_labels)
-			df = df.append(pd.DataFrame({'recall':recall,'precision':precision,'macrof1':macrof1,'microf1':microf1,'acc':acc},ignore_index=True))
+			df = df.append(pd.DataFrame({'recall':recall,'precision':precision,'macrof1':macrof1,'microf1':microf1,'acc':acc}),ignore_index=True)
 			print("[{:4d}]    recall:{:.4%}    precision:{:.4%}    macrof1:{:.4%}    microf1:{:.4%}    accuracy:{:.4%}".format(epoch, recall, precision, macrof1, microf1, acc))
 		
 		df.transpose().to_csv("metrics.csv")
