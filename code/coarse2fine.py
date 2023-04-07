dbce9f1e0445186ebf8aba8001893249e03caa84
Mahdi Kleit <46169379+Kleitoun@users.noreply.github.com>
2023-04-07 13:13:13 -0400

    Update coarse2fine.py

--- code/coarse2fine.py
+++ code/coarse2fine.py
@@ -140,7 +140,7 @@ class C2F(torch.nn.Module):
 		for epoch in range(100):
 			optimizer.zero_grad()
 
-			print(len(train_x1))
+			print(str(len(train_x1))+"HELLO")
 			
 			rand_index_x1 = np.random.choice(len(train_x1), size=32, replace=False)
 
